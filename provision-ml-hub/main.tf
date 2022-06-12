d9ca312ec16a511ef86567c2f2b7419d6140886e
Laurentiu Capatina <lcapatina@mantu.com>
2022-06-12 22:15:44 +0200

    1st deployment of mlhub

---
+++ provision-ml-hub/main.tf
@@ -0,0 +1,68 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.20.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.3.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.1"
+    }
+  }
+}
+
+data "terraform_remote_state" "eks" {
+  backend = "local"
+
+  config = {
+    path = "../provision-eks/terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  profile = "kube_admin"
+  region  = data.terraform_remote_state.eks.outputs.region
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+# Namespace
+resource "kubernetes_namespace" "mlhub" {
+  metadata {
+    annotations = {
+      name = "mlhub"
+    }
+    name = "mlhub"
+  }
+}
+
+resource "helm_release" "mlhub" {
+  name      = "mlhub"
+  chart     = "https://github.com/ml-tooling/ml-hub/releases/download/1.0.0/mlhub-chart-1.0.0.tgz"
+  namespace = "mlhub"
+}
